dc02d966751d7c72258df25dac93deedfa142ea6
alemao51092 <aarielmartinez1111@gmail.com>
2023-05-12 10:28:54 -0700

    first commit

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t* newnode = malloc(sizeof(binary_tree_t));
+    if (newnode != NULL)
+    {
+        newnode->left = NULL;
+        newnode->right = NULL;
+        newnode->parent = parent;
+        newnode->n = value;
+    }
+    return (newnode);
+    
+}
+
