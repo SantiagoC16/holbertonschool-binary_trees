9eb92bdc71ee1a12e6841d2868cd304b25f883ba
alemao51092 <aarielmartinez1111@gmail.com>
2023-05-15 07:04:53 -0700

    testing task 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *NEWNODE = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+
+	NEWNODE = malloc(sizeof(binary_tree_t));
+	if (NEWNODE == NULL)
+		return (NULL);
+
+	NEWNODE->n = value;
+	NEWNODE->parent = parent;
+	NEWNODE->left = NULL;
+
+	if (parent->right != NULL)
+	{
+		NEWNODE->right = parent->right;
+		parent->right->parent = NEWNODE;
+	}
+	else
+		NEWNODE->right = NULL;
+
+	parent->right = NEWNODE;
+
+	return (NEWNODE);
+}
