769da4db7ccd954c49737a6e2982b0dd427d5074
alemao51092 <aarielmartinez1111@gmail.com>
2023-05-16 11:32:11 -0700

    18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,20 +3,9 @@
 binary_tree_t
 *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *_parent = node->parent;
-	binary_tree_t *grandparent = _parent->parent;
-	binary_tree_t *uncle = NULL;
-
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
-
-	if (_parent == grandparent->left)
-		uncle = grandparent->right;
-	else
-		uncle = grandparent->left;
-	if (uncle)
-		return (uncle);
-	else
-		return (NULL);
-	
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
