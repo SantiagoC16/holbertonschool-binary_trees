6ca8a8829f9189d72f34cd023030be56b01fad33
alemao51092 <aarielmartinez1111@gmail.com>
2023-05-16 11:21:34 -0700

    task 18 try 20

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -5,18 +5,18 @@ binary_tree_t
 {
 	binary_tree_t *_parent = node->parent;
 	binary_tree_t *grandparent = _parent->parent;
+	binary_tree_t *uncle = NULL;
 
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
 	if (_parent == grandparent->left)
-	{
-		if (grandparent->right)
-			return (grandparent->right);
-		else
-			return (NULL);
-	}
+		uncle = grandparent->right;
 	else
-		return (grandparent->left);
+		uncle = grandparent->left;
+	if (uncle)
+		return (uncle);
+	else
+		return (NULL);
 	
 }
