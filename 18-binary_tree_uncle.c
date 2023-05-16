a6853240a3628e7bbe8c18a46c250c4faab55b4b
alemao51092 <aarielmartinez1111@gmail.com>
2023-05-16 10:15:26 -0700

    task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+binary_tree_t
+*binary_tree_uncle(binary_tree_t *node)
+{
+
+
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	binary_tree_t *parent = node->parent;
+	binary_tree_t *grandparent = parent->parent;
+
+	if (node == parent->right)
+		return (grandparent->left);
+	else
+		return (grandparent->right);
+}
