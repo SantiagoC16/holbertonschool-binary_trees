31722b511af5f0a23cfc8fdf6ffb2ee328276559
SantiagoC16 <6317@holbertonstudents.com>
2023-05-16 16:52:08 -0700

    task 15 test 2

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,22 +8,18 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    int left = 0, right = 0;
 
     if (tree == NULL)
         return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-    {
-        return (0);
-    }
-    
     if (tree->left != NULL && tree->right != NULL)
     {
-        left = binary_tree_is_full(tree->left);
-        right = binary_tree_is_full(tree->right);
-        return (left - right);
+        return (1);
     }
-
+    
+    if (tree->left && tree->right)
+    {
+        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+    }
     return (0);
 }
