0458ba61606c99755459c7ea81d3bce126f92236
SantiagoC16 <6317@holbertonstudents.com>
2023-05-16 18:17:55 -0700

    task 16 and 17 portotype

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,10 @@
 #include "binary_trees.h"
 
-/***/
-
+/**
+ * binary_tree_is_perfect - a function that checks if a binary tree is perfect
+ * @tree: is a pointer to the root node of the tree to check
+ * Return: 0 if isn't perfect, 1 if it is
+ */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
@@ -9,12 +12,35 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     if (tree == NULL)
         return (0);
 
-    if (tree->left != NULL && tree->right != NULL)
-        return (1);
-    
-    if (tree->left && tree->right)
+    if (binarytreeheight(tree->left) == binarytreeheight(tree->right))
     {
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+        if (tree->left == NULL && tree->right == NULL)
+		return (1);
+        else
+            return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
     }
+
     return (0);
+}
+
+/**
+ * binarytreeheight - aux function
+ * @tree: a pointer to the root node
+ * Return: height
+ */
+
+size_t binarytreeheight(const binary_tree_t *tree)
+{
+    int leftheight = 0, rightheight = 0;
+
+    if (tree == NULL)
+		return (0);
+    
+    leftheight = binarytreeheight(tree->left);
+	rightheight = binarytreeheight(tree->right);
+
+    if (leftheight >= rightheight)
+		return (leftheight + 1);
+	else
+		return (rightheight + 1);
 }
